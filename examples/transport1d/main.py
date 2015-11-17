2d18b4b517aff0933f66881d0d000162d4c2ff81
Arthur Talpaert <arthur.talpaert@cea.fr>
2015-11-17 11:05:34 +0100

    Formatting with PEP8 style.

--- examples/transport1d/main.py
+++ examples/transport1d/main.py
@@ -7,53 +7,53 @@ import cdmath
 
 
 def main():
-    a=-5.0;
-    b=5.0;
-    nx=1000;
-    ntmax=1000;
-    dx = (b-a)/nx;
-    pi=3.1415927;
+    a = -5.0
+    b = 5.0
+    nx = 1000
+    ntmax = 1000
+    dx = (b - a) / nx
+    pi = 3.1415927
     # Transport velocity
-    cfl=0.5;
-    u=3.;
-    dt=cfl*dx/u;
+    cfl = 0.5
+    u = 3.
+    dt = cfl * dx / u
 
-    my_mesh=cdmath.Mesh(a,b,nx);
-    conc=cdmath.Field("Concentration",cdmath.CELLS,my_mesh,1);
+    my_mesh = cdmath.Mesh(a, b, nx)
+    conc = cdmath.Field("Concentration", cdmath.CELLS, my_mesh, 1)
 
     # Initial conditions
-    sigma=math.sqrt(0.2);
+    sigma = math.sqrt(0.2)
     for i in xrange(my_mesh.getNumberOfCells()):
-        x=my_mesh.getCell(i).x();
-        conc[i] = 0.5/(sigma*math.sqrt(2*pi))*math.exp(-0.5*math.pow((x/sigma),2));
+        x = my_mesh.getCell(i).x()
+        conc[i] = 0.5 / (sigma * math.sqrt(2 * pi)) * math.exp(-0.5 * math.pow((x / sigma), 2))
         pass
 
-    time=0.;
-    tmax=3.0;
-    it=0;
+    time = 0.
+    tmax = 3.0
+    it = 0
 
     print("MED post-treatment of the solution at T=" + str(time) + "…")
-    output_filename="EqTr1D";
-    conc.setTime(time,it);
-    conc.writeMED(output_filename);
-    conc.writeVTK(output_filename);
-    conc.writeCSV(output_filename);
-    output_freq=10;
+    output_filename = "EqTr1D"
+    conc.setTime(time, it)
+    conc.writeMED(output_filename)
+    conc.writeVTK(output_filename)
+    conc.writeCSV(output_filename)
+    output_freq = 10
 
     # Time loop
-    while (it<ntmax and time <= tmax):
+    while (it < ntmax and time <= tmax):
         print("-- Iter: " + str(it) + ", Time: " + str(time) + ", dt: " + str(dt))
-        conc[0] = conc[0]- u*dt/dx*(conc[0]-conc[my_mesh.getNumberOfCells()-1]);
+        conc[0] = conc[0] - u * dt / dx * (conc[0] - conc[my_mesh.getNumberOfCells() - 1])
         for j in xrange(1, my_mesh.getNumberOfCells()):
-            conc[j] = conc[j] -u*dt/dx*(conc[j]-conc[j-1]);
+            conc[j] = conc[j] - u * dt / dx * (conc[j] - conc[j - 1])
             pass
-        time+=dt;
-        it+=1;
-        if (it%output_freq==0):
-            conc.setTime(time,it);
-            conc.writeMED(output_filename,False);
-            conc.writeVTK(output_filename,False);
-            conc.writeCSV(output_filename);
+        time += dt
+        it += 1
+        if (it % output_freq == 0):
+            conc.setTime(time, it)
+            conc.writeMED(output_filename, False)
+            conc.writeVTK(output_filename, False)
+            conc.writeCSV(output_filename)
             pass
         pass
     print("CDMATH calculation done.")
--- tests/swig/linearSolverTests.py
+++ tests/swig/linearSolverTests.py
@@ -6,82 +6,83 @@ from cdmath import *
 
 
 class TestsLinearSolverSwig(unittest.TestCase):
+
     def testClassLinearSolver(self):
-        A=Matrix(2,2);
-        A[0,0]=3.;
-        A[0,1]=-2.;
-        A[1,0]=-2.;
-        A[1,1]=4.;
- 
-        A*=A.transpose();
- 
-        Xana=Vector(2);
-        Xana[0]=1.;
-        Xana[1]=2.;
- 
-        B=A*Xana;
+        A = Matrix(2, 2)
+        A[0, 0] = 3.
+        A[0, 1] = -2.
+        A[1, 0] = -2.
+        A[1, 1] = 4.
+
+        A *= A.transpose()
+
+        Xana = Vector(2)
+        Xana[0] = 1.
+        Xana[1] = 2.
+
+        B = A * Xana
 #        LS=LinearSolver(A,B,500,1.E-10,"GMRES","LU");
 #        X=LS.solve();
 #        self.assertTrue(abs(X[0]-Xana[0])<1.E-10);
 #        self.assertTrue(abs(X[1]-Xana[1])<1.E-10);
-# 
+#
 #        self.assertEqual(LS.getStatus(),True);
- 
+
 #        self.assertEqual(LS.getNumberMaxOfIter(),500);
 #        self.assertEqual(LS.getTolerance(),1.E-10);
 #        self.assertEqual(LS.getNameOfMethod(),"GMRES");
 #        self.assertEqual(LS.getNumberOfIter(),1);
 #        self.assertEqual(LS.isSingular(),False);
 #        self.assertEqual(LS.getNameOfPc(),"LU");
-# 
-        LS2=LinearSolver(A,B,500,1.E-10,"CG");
-        A1=Matrix(2,2);
-        A1[0,0]=1.;
-        A1[0,1]=-2.;
-        A1[1,0]=-2.;
-        A1[1,1]=4.;
- 
-        LS2.setMatrix(A1*-1.);
-        LS2.setSndMember(B*-1);
-        LS2.setTolerance(1.E-20);
-        LS2.setNumberMaxOfIter(10);
-        LS2.setSingularity(True);
-        X2=LS2.solve();
-        self.assertTrue(abs(X2[0]-(-4.55555555556))<1.E-10);
-        self.assertTrue(abs(X2[1]-4.55555555556)<1.E-10);
-        self.assertEqual(LS2.getStatus(),True);
-        self.assertEqual(LS2.getNumberOfIter(),2);
-        self.assertEqual(LS2.isSingular(),True);
-        self.assertEqual(LS2.getNameOfMethod(),"CG");
-# 
-        LS3=LinearSolver(A,B,500,1.E-10,"BCG");
-        X3=LS3.solve();
-        self.assertTrue(abs(X3[0]-Xana[0])<1.E-10);
-        self.assertTrue(abs(X3[1]-Xana[1])<1.E-10);
-        self.assertEqual(LS3.getStatus(),True);
-        self.assertEqual(LS3.getNameOfMethod(),"BCG");
-# 
-        LS3=LinearSolver(A,B,500,1.E-10,"CR");
-        X3=LS3.solve();
-        self.assertTrue(abs(X3[0]-Xana[0])<1.E-10);
-        self.assertTrue(abs(X3[1]-Xana[1])<1.E-10);
-        self.assertEqual(LS3.getStatus(),True);
-        self.assertEqual(LS3.getNameOfMethod(),"CR");
-# 
-        LS3=LinearSolver(A,B,500,1.E-10,"CGS");
-        X3=LS3.solve();
-        self.assertTrue(abs(X3[0]-Xana[0])<1.E-10);
-        self.assertTrue(abs(X3[1]-Xana[1])<1.E-10);
-        self.assertEqual(LS3.getStatus(),True);
-        self.assertEqual(LS3.getNameOfMethod(),"CGS");
- 
-        LS3=LinearSolver(A,B,500,1.E-10,"GMRES");
-        X3=LS3.solve();
-        self.assertTrue(abs(X3[0]-Xana[0])<1.E-10);
-        self.assertTrue(abs(X3[1]-Xana[1])<1.E-10);
-        self.assertEqual(LS3.getStatus(),True);
-        self.assertEqual(LS3.getNameOfMethod(),"GMRES");
- 
+#
+        LS2 = LinearSolver(A, B, 500, 1.E-10, "CG")
+        A1 = Matrix(2, 2)
+        A1[0, 0] = 1.
+        A1[0, 1] = -2.
+        A1[1, 0] = -2.
+        A1[1, 1] = 4.
+
+        LS2.setMatrix(A1 * -1.)
+        LS2.setSndMember(B * -1)
+        LS2.setTolerance(1.E-20)
+        LS2.setNumberMaxOfIter(10)
+        LS2.setSingularity(True)
+        X2 = LS2.solve()
+        self.assertTrue(abs(X2[0] - (-4.55555555556)) < 1.E-10)
+        self.assertTrue(abs(X2[1] - 4.55555555556) < 1.E-10)
+        self.assertEqual(LS2.getStatus(), True)
+        self.assertEqual(LS2.getNumberOfIter(), 2)
+        self.assertEqual(LS2.isSingular(), True)
+        self.assertEqual(LS2.getNameOfMethod(), "CG")
+#
+        LS3 = LinearSolver(A, B, 500, 1.E-10, "BCG")
+        X3 = LS3.solve()
+        self.assertTrue(abs(X3[0] - Xana[0]) < 1.E-10)
+        self.assertTrue(abs(X3[1] - Xana[1]) < 1.E-10)
+        self.assertEqual(LS3.getStatus(), True)
+        self.assertEqual(LS3.getNameOfMethod(), "BCG")
+#
+        LS3 = LinearSolver(A, B, 500, 1.E-10, "CR")
+        X3 = LS3.solve()
+        self.assertTrue(abs(X3[0] - Xana[0]) < 1.E-10)
+        self.assertTrue(abs(X3[1] - Xana[1]) < 1.E-10)
+        self.assertEqual(LS3.getStatus(), True)
+        self.assertEqual(LS3.getNameOfMethod(), "CR")
+#
+        LS3 = LinearSolver(A, B, 500, 1.E-10, "CGS")
+        X3 = LS3.solve()
+        self.assertTrue(abs(X3[0] - Xana[0]) < 1.E-10)
+        self.assertTrue(abs(X3[1] - Xana[1]) < 1.E-10)
+        self.assertEqual(LS3.getStatus(), True)
+        self.assertEqual(LS3.getNameOfMethod(), "CGS")
+
+        LS3 = LinearSolver(A, B, 500, 1.E-10, "GMRES")
+        X3 = LS3.solve()
+        self.assertTrue(abs(X3[0] - Xana[0]) < 1.E-10)
+        self.assertTrue(abs(X3[1] - Xana[1]) < 1.E-10)
+        self.assertEqual(LS3.getStatus(), True)
+        self.assertEqual(LS3.getNameOfMethod(), "GMRES")
+
 #        LS3=LinearSolver(A,B,500,1.E-10,"BICG","LU");
 #        X3=LS3.solve();
 #        self.assertTrue(abs(X3[0]-Xana[0])<1.E-10);
@@ -89,35 +90,35 @@ class TestsLinearSolverSwig(unittest.TestCase):
 #        self.assertEqual(LS3.getStatus(),True);
 #        self.assertEqual(LS3.getNameOfMethod(),"BICG");
 #        self.assertEqual(LS3.getNameOfPc(),"LU");
-# 
-        LS3=LinearSolver(A,B,500,1.E-10,"BICG");
-        X3=LS3.solve();
-        self.assertTrue(abs(X3[0]-Xana[0])<1.E-10);
-        self.assertTrue(abs(X3[1]-Xana[1])<1.E-10);
-        self.assertEqual(LS3.getStatus(),True);
-        self.assertEqual(LS3.getNameOfMethod(),"BICG");
- 
-        LS3=LinearSolver(A,B,500,1.E-10,"GCR");
-        X3=LS3.solve();
-        self.assertTrue(abs(X3[0]-Xana[0])<1.E-10);
-        self.assertTrue(abs(X3[1]-Xana[1])<1.E-10);
-        self.assertEqual(LS3.getStatus(),True);
-        self.assertEqual(LS3.getNameOfMethod(),"GCR");
- 
-        LS3=LinearSolver(A,B,500,1.E-10,"LSQR");
-        X3=LS3.solve();
-        self.assertTrue(abs(X3[0]-Xana[0])<1.E-10);
-        self.assertTrue(abs(X3[1]-Xana[1])<1.E-10);
-        self.assertEqual(LS3.getStatus(),True);
-        self.assertEqual(LS3.getNameOfMethod(),"LSQR");
-# 
-        LS3=LinearSolver(A,B,500,1.E-10,"CHOLESKY");
-        X3=LS3.solve();
-        self.assertTrue(abs(X3[0]-Xana[0])<1.E-10);
-        self.assertTrue(abs(X3[1]-Xana[1])<1.E-10);
-        self.assertEqual(LS3.getStatus(),True);
-        self.assertEqual(LS3.getNameOfMethod(),"CHOLESKY");
- 
+#
+        LS3 = LinearSolver(A, B, 500, 1.E-10, "BICG")
+        X3 = LS3.solve()
+        self.assertTrue(abs(X3[0] - Xana[0]) < 1.E-10)
+        self.assertTrue(abs(X3[1] - Xana[1]) < 1.E-10)
+        self.assertEqual(LS3.getStatus(), True)
+        self.assertEqual(LS3.getNameOfMethod(), "BICG")
+
+        LS3 = LinearSolver(A, B, 500, 1.E-10, "GCR")
+        X3 = LS3.solve()
+        self.assertTrue(abs(X3[0] - Xana[0]) < 1.E-10)
+        self.assertTrue(abs(X3[1] - Xana[1]) < 1.E-10)
+        self.assertEqual(LS3.getStatus(), True)
+        self.assertEqual(LS3.getNameOfMethod(), "GCR")
+
+        LS3 = LinearSolver(A, B, 500, 1.E-10, "LSQR")
+        X3 = LS3.solve()
+        self.assertTrue(abs(X3[0] - Xana[0]) < 1.E-10)
+        self.assertTrue(abs(X3[1] - Xana[1]) < 1.E-10)
+        self.assertEqual(LS3.getStatus(), True)
+        self.assertEqual(LS3.getNameOfMethod(), "LSQR")
+#
+        LS3 = LinearSolver(A, B, 500, 1.E-10, "CHOLESKY")
+        X3 = LS3.solve()
+        self.assertTrue(abs(X3[0] - Xana[0]) < 1.E-10)
+        self.assertTrue(abs(X3[1] - Xana[1]) < 1.E-10)
+        self.assertEqual(LS3.getStatus(), True)
+        self.assertEqual(LS3.getNameOfMethod(), "CHOLESKY")
+
 #        LS3=LinearSolver(A,B,500,1.E-10,"LU");
 #        X3=LS3.solve();
 #        self.assertTrue(abs(X3[0]-Xana[0])<1.E-10);
@@ -125,147 +126,147 @@ class TestsLinearSolverSwig(unittest.TestCase):
 #        self.assertEqual(LS3.getStatus(),True);
 #        self.assertEqual(LS3.getNameOfMethod(),"LU");
 #        self.assertEqual(LS3.getNameOfPc(),"");
- 
-        A2=SparseMatrix(6,6,16);
-        A2[0,0]=2.;
-        A2[0,1]=-1.;
- 
-        A2[1,0]=-1.;
-        A2[1,1]=2.;
-        A2[1,2]=-1.;
- 
-        A2[2,1]=-1.;
-        A2[2,2]=2.;
-        A2[2,3]=-1.;
- 
-        A2[3,2]=-1.;
-        A2[3,3]=2.;
-        A2[3,4]=-1.;
- 
-        A2[4,3]=-1.;
-        A2[4,4]=2.;
-        A2[4,5]=-1.;
- 
-        A2[5,4]=-1.;
-        A2[5,5]=2.;
- 
-        Xana2=Vector(6);
-        Xana2[0]=1.;
-        Xana2[1]=2.;
-        Xana2[2]=3.;
-        Xana2[3]=4.;
-        Xana2[4]=5.;
-        Xana2[5]=6.;
- 
-        B2=A2*Xana2;
- 
-        LS11=LinearSolver(A2,B2,500,1.E-10,"GMRES","ILU");
-        X11=LS11.solve();
+
+        A2 = SparseMatrix(6, 6, 16)
+        A2[0, 0] = 2.
+        A2[0, 1] = -1.
+
+        A2[1, 0] = -1.
+        A2[1, 1] = 2.
+        A2[1, 2] = -1.
+
+        A2[2, 1] = -1.
+        A2[2, 2] = 2.
+        A2[2, 3] = -1.
+
+        A2[3, 2] = -1.
+        A2[3, 3] = 2.
+        A2[3, 4] = -1.
+
+        A2[4, 3] = -1.
+        A2[4, 4] = 2.
+        A2[4, 5] = -1.
+
+        A2[5, 4] = -1.
+        A2[5, 5] = 2.
+
+        Xana2 = Vector(6)
+        Xana2[0] = 1.
+        Xana2[1] = 2.
+        Xana2[2] = 3.
+        Xana2[3] = 4.
+        Xana2[4] = 5.
+        Xana2[5] = 6.
+
+        B2 = A2 * Xana2
+
+        LS11 = LinearSolver(A2, B2, 500, 1.E-10, "GMRES", "ILU")
+        X11 = LS11.solve()
         for i in xrange(X11.getNumberOfRows()):
-            self.assertTrue(abs(X11[i]-Xana2[i])<1.E-10);
+            self.assertTrue(abs(X11[i] - Xana2[i]) < 1.E-10)
             pass
- 
-        self.assertEqual(LS11.getStatus(),True);
- 
-        self.assertEqual(LS11.getNumberMaxOfIter(),500);
-        self.assertEqual(LS11.getTolerance(),1.E-10);
-        self.assertEqual(LS11.getNumberOfIter(),1);
- 
-        LS11=LinearSolver(A2,B2,500,1.E-10,"CG","ILU");
-        X11=LS11.solve();
+
+        self.assertEqual(LS11.getStatus(), True)
+
+        self.assertEqual(LS11.getNumberMaxOfIter(), 500)
+        self.assertEqual(LS11.getTolerance(), 1.E-10)
+        self.assertEqual(LS11.getNumberOfIter(), 1)
+
+        LS11 = LinearSolver(A2, B2, 500, 1.E-10, "CG", "ILU")
+        X11 = LS11.solve()
         for i in xrange(X11.getNumberOfRows()):
-            self.assertTrue(abs(X11[i]-Xana2[i])<1.E-10);
+            self.assertTrue(abs(X11[i] - Xana2[i]) < 1.E-10)
             pass
- 
-        self.assertEqual(LS11.getStatus(),True);
- 
-        self.assertEqual(LS11.getNumberMaxOfIter(),500);
-        self.assertEqual(LS11.getTolerance(),1.E-10);
-        self.assertEqual(LS11.getNumberOfIter(),1);
- 
-        LS11=LinearSolver(A2,B2,500,1.E-10,"LGMRES","ILU");
-        X11=LS11.solve();
+
+        self.assertEqual(LS11.getStatus(), True)
+
+        self.assertEqual(LS11.getNumberMaxOfIter(), 500)
+        self.assertEqual(LS11.getTolerance(), 1.E-10)
+        self.assertEqual(LS11.getNumberOfIter(), 1)
+
+        LS11 = LinearSolver(A2, B2, 500, 1.E-10, "LGMRES", "ILU")
+        X11 = LS11.solve()
         for i in xrange(X11.getNumberOfRows()):
-            self.assertTrue(abs(X11[i]-Xana2[i])<1.E-10);
+            self.assertTrue(abs(X11[i] - Xana2[i]) < 1.E-10)
             pass
- 
-        self.assertEqual(LS11.getStatus(),True);
- 
-        self.assertEqual(LS11.getNumberMaxOfIter(),500);
-        self.assertEqual(LS11.getTolerance(),1.E-10);
-        self.assertEqual(LS11.getNumberOfIter(),1);
- 
-        LS11=LinearSolver(A2,B2,500,1.E-10,"BCG","ILU");
-        X11=LS11.solve();
+
+        self.assertEqual(LS11.getStatus(), True)
+
+        self.assertEqual(LS11.getNumberMaxOfIter(), 500)
+        self.assertEqual(LS11.getTolerance(), 1.E-10)
+        self.assertEqual(LS11.getNumberOfIter(), 1)
+
+        LS11 = LinearSolver(A2, B2, 500, 1.E-10, "BCG", "ILU")
+        X11 = LS11.solve()
         for i in xrange(X11.getNumberOfRows()):
-            self.assertTrue(abs(X11[i]-Xana2[i])<1.E-10);
+            self.assertTrue(abs(X11[i] - Xana2[i]) < 1.E-10)
             pass
- 
-        self.assertEqual(LS11.getStatus(),True);
- 
-        self.assertEqual(LS11.getNumberMaxOfIter(),500);
-        self.assertEqual(LS11.getTolerance(),1.E-10);
-        self.assertEqual(LS11.getNumberOfIter(),1);
- 
-        LS11=LinearSolver(A2,B2,500,1.E-10,"CR","ILU");
-        X11=LS11.solve();
+
+        self.assertEqual(LS11.getStatus(), True)
+
+        self.assertEqual(LS11.getNumberMaxOfIter(), 500)
+        self.assertEqual(LS11.getTolerance(), 1.E-10)
+        self.assertEqual(LS11.getNumberOfIter(), 1)
+
+        LS11 = LinearSolver(A2, B2, 500, 1.E-10, "CR", "ILU")
+        X11 = LS11.solve()
         for i in xrange(X11.getNumberOfRows()):
-            self.assertTrue(abs(X11[i]-Xana2[i])<1.E-10);
+            self.assertTrue(abs(X11[i] - Xana2[i]) < 1.E-10)
             pass
- 
-        self.assertEqual(LS11.getStatus(),True);
- 
-        self.assertEqual(LS11.getNumberMaxOfIter(),500);
-        self.assertEqual(LS11.getTolerance(),1.E-10);
-        self.assertEqual(LS11.getNumberOfIter(),1);
- 
-        LS11=LinearSolver(A2,B2,500,1.E-10,"CGS","ILU");
-        X11=LS11.solve();
+
+        self.assertEqual(LS11.getStatus(), True)
+
+        self.assertEqual(LS11.getNumberMaxOfIter(), 500)
+        self.assertEqual(LS11.getTolerance(), 1.E-10)
+        self.assertEqual(LS11.getNumberOfIter(), 1)
+
+        LS11 = LinearSolver(A2, B2, 500, 1.E-10, "CGS", "ILU")
+        X11 = LS11.solve()
         for i in xrange(X11.getNumberOfRows()):
-            self.assertTrue(abs(X11[i]-Xana2[i])<1.E-10);
+            self.assertTrue(abs(X11[i] - Xana2[i]) < 1.E-10)
             pass
- 
-        self.assertEqual(LS11.getStatus(),True);
- 
-        self.assertEqual(LS11.getNumberMaxOfIter(),500);
-        self.assertEqual(LS11.getTolerance(),1.E-10);
-        self.assertEqual(LS11.getNumberOfIter(),1);
- 
-        LS11=LinearSolver(A2,B2,500,1.E-10,"BICG","ILU");
-        X11=LS11.solve();
+
+        self.assertEqual(LS11.getStatus(), True)
+
+        self.assertEqual(LS11.getNumberMaxOfIter(), 500)
+        self.assertEqual(LS11.getTolerance(), 1.E-10)
+        self.assertEqual(LS11.getNumberOfIter(), 1)
+
+        LS11 = LinearSolver(A2, B2, 500, 1.E-10, "BICG", "ILU")
+        X11 = LS11.solve()
         for i in xrange(X11.getNumberOfRows()):
-            self.assertTrue(abs(X11[i]-Xana2[i])<1.E-10);
+            self.assertTrue(abs(X11[i] - Xana2[i]) < 1.E-10)
             pass
- 
-        self.assertEqual(LS11.getStatus(),True);
- 
-        self.assertEqual(LS11.getNumberMaxOfIter(),500);
-        self.assertEqual(LS11.getTolerance(),1.E-10);
-        self.assertEqual(LS11.getNumberOfIter(),1);
- 
-        LS11=LinearSolver(A2,B2,500,1.E-10,"GCR","ILU");
-        X11=LS11.solve();
+
+        self.assertEqual(LS11.getStatus(), True)
+
+        self.assertEqual(LS11.getNumberMaxOfIter(), 500)
+        self.assertEqual(LS11.getTolerance(), 1.E-10)
+        self.assertEqual(LS11.getNumberOfIter(), 1)
+
+        LS11 = LinearSolver(A2, B2, 500, 1.E-10, "GCR", "ILU")
+        X11 = LS11.solve()
         for i in xrange(X11.getNumberOfRows()):
-            self.assertTrue(abs(X11[i]-Xana2[i])<1.E-10);
+            self.assertTrue(abs(X11[i] - Xana2[i]) < 1.E-10)
             pass
- 
-        self.assertEqual(LS11.getStatus(),True);
- 
-        self.assertEqual(LS11.getNumberMaxOfIter(),500);
-        self.assertEqual(LS11.getTolerance(),1.E-10);
-        self.assertEqual(LS11.getNumberOfIter(),1);
- 
-        LS11=LinearSolver(A2,B2,500,1.E-10,"LSQR","ILU");
-        X11=LS11.solve();
+
+        self.assertEqual(LS11.getStatus(), True)
+
+        self.assertEqual(LS11.getNumberMaxOfIter(), 500)
+        self.assertEqual(LS11.getTolerance(), 1.E-10)
+        self.assertEqual(LS11.getNumberOfIter(), 1)
+
+        LS11 = LinearSolver(A2, B2, 500, 1.E-10, "LSQR", "ILU")
+        X11 = LS11.solve()
         for i in xrange(X11.getNumberOfRows()):
-            self.assertTrue(abs(X11[i]-Xana2[i])<1.E-10);
+            self.assertTrue(abs(X11[i] - Xana2[i]) < 1.E-10)
             pass
- 
-        self.assertEqual(LS11.getStatus(),True);
- 
-        self.assertEqual(LS11.getNumberMaxOfIter(),500);
-        self.assertEqual(LS11.getTolerance(),1.E-10);
-        self.assertEqual(LS11.getNumberOfIter(),6);
+
+        self.assertEqual(LS11.getStatus(), True)
+
+        self.assertEqual(LS11.getNumberMaxOfIter(), 500)
+        self.assertEqual(LS11.getTolerance(), 1.E-10)
+        self.assertEqual(LS11.getNumberOfIter(), 6)
 
 if __name__ == '__main__':
     unittest.main()
