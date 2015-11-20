f6ac7177d054d5d54cd62e60671df41e95927837
Arthur Talpaert <arthur.talpaert@cea.fr>
2015-11-20 10:25:07 +0100

    FindCDMATH.cmake more precise and with more documentation.

--- cmake_files/FindCDMATH.cmake
+++ cmake_files/FindCDMATH.cmake
@@ -1,3 +1,44 @@
+##Copyright (C) arthurtalpaert.
+##All rights reserved.
+##
+##Redistribution and use in source and binary forms, with or without modification,
+##are permitted provided that the following conditions are met:
+##
+##* Redistributions of source code must retain the above copyright notice, this
+##  list of conditions and the following disclaimer.
+##
+##* Redistributions in binary form must reproduce the above copyright notice, this
+##  list of conditions and the following disclaimer in the documentation and/or
+##  other materials provided with the distribution.
+##
+##THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+##ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+##WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+##DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+##ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+##(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+##LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+##ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+##(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+##SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# - Try to find CDMATH
+# Once done this will define
+#
+#  CDMATH_FOUND        - system has CDMATH
+#  CDMATH_INCLUDES     - the CDMATH include directories
+#  CDMATH_LIBRARIES    - Link these to use CDMATH
+#
+#  Usage:
+#  find_package(CDMATH)
+#
+# Setting these changes the behavior of the search:
+#  CDMATH_DIR - directory in which CDMATH resides
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+
 find_path (CDMATH_DIR include/CdmathException.hxx
   HINTS ENV CDMATH_DIR
   PATHS
@@ -10,15 +51,23 @@ message (STATUS "Found CDMATH: ${CDMATH_DIR}")
 # Include directories
 # This sets the variable ${CDMATH_INCLUDES}.
 set(CDMATH_INCLUDES ${CDMATH_DIR}/include)
+if (NOT (IS_DIRECTORY  ${CDMATH_INCLUDES}) )
+  message (SEND_ERROR "CDMATH_INCLUDES can not be used, ${CDMATH_INCLUDES} does not exist.")
+endif () 
 
 # CDMATH libraries against which to link
 # This sets the variable ${CDMATH_LIBRARIES}.
-find_library (INTERPKERNEL_LIB interpkernel PATHS ${CDMATH_DIR}/lib)
-find_library (MEDC_LIB medC PATHS ${CDMATH_DIR}/lib)
-find_library (MEDLOADER_LIB medloader PATHS ${CDMATH_DIR}/lib)
-find_library (MEDCOUPLING_LIB medcoupling PATHS ${CDMATH_DIR}/lib)
-find_library (BASE_LIB base PATHS ${CDMATH_DIR}/lib)
-find_library (MESH_LIB mesh PATHS ${CDMATH_DIR}/lib)
-find_library (LINEARSOLVER_LIB linearsolver PATHS ${CDMATH_DIR}/lib)
+set(CDMATH_LIBDIR ${CDMATH_DIR}/lib)
+if (NOT (IS_DIRECTORY  ${CDMATH_LIBDIR}) )
+  message (SEND_ERROR "CDMATH_LIBDIR can not be used, ${CDMATH_LIBDIR} does not exist.")
+endif () 
+find_library (INTERPKERNEL_LIB NAMES interpkernel PATHS ${CDMATH_LIBDIR})
+find_library (MEDC_LIB NAMES medC PATHS ${CDMATH_LIBDIR})
+find_library (MEDLOADER_LIB NAMES medloader PATHS ${CDMATH_LIBDIR})
+find_library (MEDCOUPLING_LIB NAMES medcoupling PATHS ${CDMATH_LIBDIR})
+find_library (CDMATHBASE_LIB NAMES base PATHS ${CDMATH_LIBDIR})
+find_library (CDMATHMESH_LIB NAMES mesh PATHS ${CDMATH_LIBDIR})
+find_library (CDMATHLINEARSOLVER_LIB NAMES linearsolver PATHS ${CDMATH_LIBDIR})
 set (CDMATH_LIBRARIES
-    ${INTERPKERNEL_LIB} ${MEDC_LIB} ${MEDLOADER_LIB} ${MEDCOUPLING_LIB} ${BASE_LIB} ${MESH_LIB} ${LINEARSOLVER_LIB})
+    ${INTERPKERNEL_LIB} ${MEDC_LIB} ${MEDLOADER_LIB} ${MEDCOUPLING_LIB}
+    ${CDMATHBASE_LIB} ${CDMATHMESH_LIB} ${CDMATHLINEARSOLVER_LIB})
