16ab5852c8fb8b7be43d8e2d8a2fbb0c2480ca3e
Arthur Talpaert <arthur.talpaert_github@polytechnique.org>
2016-07-26 11:26:17 +0200

    A little more like conventions.

--- examples/poisson/finite_elements.py
+++ examples/poisson/finite_elements.py
@@ -1,144 +1,158 @@
+#!/usr/bin/env python
 # -*-coding:utf-8 -*
-#===============================================================================================================================
-# Name        : Résolution EF de l'équation de Poisson 2D -\triangle u = f avec conditions aux limites de Dirichlet u=0
-# Author      : Michaël Ndjinga
-# Copyright   : CEA Saclay 2016
-# Description : Utilisation de la méthode des éléménts finis P1 avec champs u et f discrétisés aux noeuds d'un maillage triangulaire
-#				Création et sauvegarde du champ résultant ainsi que du champ second membre en utilisant la librairie CDMATH
-#================================================================================================================================
+
+#===============================================================================
+# Name       : Résolution EF de l'équation de Poisson 2D -Δu = f
+#                  avec conditions aux limites de Dirichlet u=0.
+# Author     : Michaël Ndjinga
+# Copyright  : CEA Saclay 2016
+# Description: Utilisation de la méthode des éléménts finis P1
+#                  avec champs u et f discrétisés aux noeuds
+#                  d'un maillage triangulaire.
+#                  Création et sauvegarde du champ résultant ainsi que
+#                  du champ second membre en utilisant la librairie CDMATH.
+#===============================================================================
+
+from math import pi
+from math import sin
 
 import cdmath
-from math import sin, pi
 
-#Préprocessing optionnel: création du fichier my_mesh.med contenant la géométrie et le maillage du domaine de calcul à partir de commandes python (import salome)
+# Préprocessing optionnel : création du fichier my_mesh.med contenant
+# la géométrie et le maillage du domaine de calcul à partir de commandes Python
+# (import SALOME)
 
-#Chargement du maillage triangulaire du domaine carré [0,1]x[0,1], définition des bords
+# Chargement du maillage triangulaire du domaine carré [0,1]x[0,1], définition des bords
 #=======================================================================================
 my_mesh = cdmath.Mesh("MeshTri2600Cells.med") #need a function isTriangular/isTetrahedric to check that the cells are triangular/tetrahedric
-eps=1e-6
-my_mesh.setGroupAtPlan(0.,0,eps,"DirichletBorder")#Bord GAUCHE
-my_mesh.setGroupAtPlan(1.,0,eps,"DirichletBorder")#Bord DROIT
-my_mesh.setGroupAtPlan(0.,1,eps,"DirichletBorder")#Bord BAS
-my_mesh.setGroupAtPlan(1.,1,eps,"DirichletBorder")#Bord HAUT
-#my_mesh.setGroupAtPlan(0,2,eps,"DirichletBorder")#Bord AVANT si calcul 3D
-#my_mesh.setGroupAtPlan(1,2,eps,"DirichletBorder")#Bord ARRIERE si calcul 3D
+eps = 1e-6
+my_mesh.setGroupAtPlan(0.,0,eps,"DirichletBorder") #Bord GAUCHE
+my_mesh.setGroupAtPlan(1.,0,eps,"DirichletBorder") #Bord DROIT
+my_mesh.setGroupAtPlan(0.,1,eps,"DirichletBorder") #Bord BAS
+my_mesh.setGroupAtPlan(1.,1,eps,"DirichletBorder") #Bord HAUT
+#my_mesh.setGroupAtPlan(0,2,eps,"DirichletBorder") #Bord AVANT si calcul 3D
+#my_mesh.setGroupAtPlan(1,2,eps,"DirichletBorder") #Bord ARRIERE si calcul 3D
 
 nbNodes = my_mesh.getNumberOfNodes()
 nbCells = my_mesh.getNumberOfCells()
 
 print("Fin chargement du maillage")
-print("nb of nodes=", nbNodes)
-print("nb of cells=", nbCells)
+print("nb of nodes = ", nbNodes)
+print("nb of cells = ", nbCells)
 
-#Discrétisation du second membre et détermination des noeuds intérieurs
-#======================================================================
-my_RHSfield = cdmath.Field("RHS field", cdmath.NODES, my_mesh, 1)
+# Discrétisation du second membre et détermination des noeuds intérieurs
+#=======================================================================
+my_RHS_field = cdmath.Field("RHS field", cdmath.NODES, my_mesh, 1)
 nbInteriorNodes = 0
-nbBoundaryNodes=0
-maxNbNeighbours=0#This is to determine the number of non zero coefficients in the sparse finite element rigidity matrix
-interiorNodes=[]
-boundaryNodes=[]
+nbBoundaryNodes = 0
+maxNbNeighbours = 0#This is to determine the number of non zero coefficients in the sparse finite element rigidity matrix
+interiorNodes = []
+boundaryNodes = []
 
-#parcours des noeuds pour discrétisation du second membre et extraction 1) des noeuds intérieur 2) des noeuds frontière 3) du nb max voisins d'un noeud
+# Parcours des noeuds pour discrétisation du second membre et extraction 1) des noeuds intérieur 2) des noeuds frontière 3) du nb max voisins d'un noeud
 for i in range(nbNodes):
-	Ni=my_mesh.getNode(i)
-	x = Ni.x()
-	y = Ni.y()
-	#z=Ni.z() si calcul 3D
-	my_RHSfield[i]=2*pi*pi*sin(pi*x)*sin(pi*y)#mettre la fonction definie au second membre de l'edp
-	if my_mesh.isBorderNode(i): # Détection des noeuds frontière
-		boundaryNodes.append(i)
-		nbBoundaryNodes=nbBoundaryNodes+1
-	else: # Détection des noeuds intérieurs
-		interiorNodes.append(i)
-		nbInteriorNodes=nbInteriorNodes+1
-		maxNbNeighbours= max(1+Ni.getNumberOfCells(),maxNbNeighbours) #true only in 2D, need a function Ni.getNumberOfNeighbourNodes()
+    Ni = my_mesh.getNode(i)
+    x = Ni.x()
+    y = Ni.y()
+    #z=Ni.z() si calcul 3D
+    my_RHS_field[i] = 2*pi*pi*sin(pi*x)*sin(pi*y)#mettre la fonction definie au second membre de l'edp
+    if my_mesh.isBorderNode(i): # Détection des noeuds frontière
+        boundaryNodes.append(i)
+        nbBoundaryNodes = nbBoundaryNodes+1
+    else: # Détection des noeuds intérieurs
+        interiorNodes.append(i)
+        nbInteriorNodes = nbInteriorNodes+1
+        maxNbNeighbours = max(1+Ni.getNumberOfCells(),maxNbNeighbours) #true only in 2D, need a function Ni.getNumberOfNeighbourNodes()
 
-# sauvegarde sur le disque dur du second membre discrétisé dans un fichier paraview
-my_RHSfield.writeVTK("FiniteElementsRHSField")
+# Sauvegarde sur le disque dur du second membre discrétisé dans un fichier paraview
+my_RHS_field.writeVTK("FiniteElementsRHSField")
 
 print("Fin discrétisation second membdre de l'edp")
-print("nb of interior nodes=", nbInteriorNodes)
-print("nb of boundary nodes=", nbBoundaryNodes)
-print("Max nb of neighbours=", maxNbNeighbours)
+print("nb of interior nodes = ", nbInteriorNodes)
+print("nb of boundary nodes = ", nbBoundaryNodes)
+print("Max nb of neighbours = ", maxNbNeighbours)
 
-# Construction de la matrice de rigidité et du vecteur second membre du système linéaire
-#=======================================================================================
-Rigidite=cdmath.SparseMatrix(nbInteriorNodes,nbInteriorNodes,nbInteriorNodes*maxNbNeighbours)
-RHS=cdmath.Vector(nbInteriorNodes)
+# Construction de la matrice de rigidité et du vecteur second membre du
+#     système linéaire
+#======================================================================
+rigidite = cdmath.SparseMatrix(nbInteriorNodes,nbInteriorNodes,
+    nbInteriorNodes*maxNbNeighbours)
+RHS = cdmath.Vector(nbInteriorNodes)
 
-# Vecteurs gradient de la fonction de forme associée à chaque noeud d'un triangle (hypothèse 2D)
-GradShapeFunc0=cdmath.Vector(2)#en 3D GradShapeFunc0=cdmath.Vector(3)
-GradShapeFunc1=cdmath.Vector(2)#en 3D GradShapeFunc1=cdmath.Vector(3)
-GradShapeFunc2=cdmath.Vector(2)#en 3D GradShapeFunc2=cdmath.Vector(3)
+# Vecteurs gradient de la fonction de forme associée à chaque noeud d'un
+#     triangle (hypothèse 2D)
+GradShapeFunc0 = cdmath.Vector(2)#en 3D GradShapeFunc0=cdmath.Vector(3)
+GradShapeFunc1 = cdmath.Vector(2)#en 3D GradShapeFunc1=cdmath.Vector(3)
+GradShapeFunc2 = cdmath.Vector(2)#en 3D GradShapeFunc2=cdmath.Vector(3)
 #GradShapeFunc3=cdmath.Vector(3) si calcul 3D
 
-#On parcourt les triangles du domaine
+# On parcourt les triangles du domaine
 for i in range(nbCells):
+    Ci = my_mesh.getCell(i)
+    #Contribution à la matrice de rigidité
+    nodeId0 = Ci.getNodeId(0)
+    nodeId1 = Ci.getNodeId(1)
+    nodeId2 = Ci.getNodeId(2)
+    #nodeId3=Ci.getNodeId(3) si calcul 3D
+    N0 = my_mesh.getNode(nodeId0)
+    N1 = my_mesh.getNode(nodeId1)
+    N2 = my_mesh.getNode(nodeId2)
+    #N3=my_mesh.getNode(nodeId3) si calcul 3D
+    #Formule des gradients voir EF P1 -> calcul déterminants
+    GradShapeFunc0[0] = (N1.y()-N2.y())/2
+    GradShapeFunc0[1] = -(N1.x()-N2.x())/2
+    GradShapeFunc1[0] = -(N0.y()-N2.y())/2
+    GradShapeFunc1[1] = (N0.x()-N2.x())/2
+    GradShapeFunc2[0] = (N0.y()-N1.y())/2
+    GradShapeFunc2[1] = -(N0.x()-N1.x())/2
+    #En 3D l'expression du déterminant est plus complexe
+    #    et il faut remplir le vecteur GradShapeFunc3
+    #Création d'un tableau (numéro du noeud, gradient de la fonction de forme
+    GradShapeFuncs={nodeId0 : GradShapeFunc0}
+    GradShapeFuncs[nodeId1]=GradShapeFunc1
+    GradShapeFuncs[nodeId2]=GradShapeFunc2
+    #GradShapeFuncs[nodeId3]=GradShapeFunc3 en 3D
 
-	Ci=my_mesh.getCell(i)
-	#Contribution à la matrice de rigidité
-	nodeId0=Ci.getNodeId(0)
-	nodeId1=Ci.getNodeId(1)
-	nodeId2=Ci.getNodeId(2)
-	#nodeId3=Ci.getNodeId(3) si calcul 3D
-	N0=my_mesh.getNode(nodeId0)
-	N1=my_mesh.getNode(nodeId1)
-	N2=my_mesh.getNode(nodeId2)
-	#N3=my_mesh.getNode(nodeId3) si calcul 3D
-	#Formule des gradients voir EF P1 -> calcul déterminants
-	GradShapeFunc0[0]= (N1.y()-N2.y())/2
-	GradShapeFunc0[1]=-(N1.x()-N2.x())/2
-	GradShapeFunc1[0]=-(N0.y()-N2.y())/2
-	GradShapeFunc1[1]= (N0.x()-N2.x())/2
-	GradShapeFunc2[0]= (N0.y()-N1.y())/2
-	GradShapeFunc2[1]=-(N0.x()-N1.x())/2
-	#En 3D l'expression du déterminant est plus complexe et il faut remplir le vecteur GradShapeFunc3
-	#Création d'un tableau (numéro du noeud, gradient de la fonction de forme
-	GradShapeFuncs={nodeId0 : GradShapeFunc0}
-	GradShapeFuncs[nodeId1]=GradShapeFunc1
-	GradShapeFuncs[nodeId2]=GradShapeFunc2
-	#GradShapeFuncs[nodeId3]=GradShapeFunc3 en 3D
-
-	# Remplissage de  la matrice de rigidité et du second membre
-	for j in [nodeId0,nodeId1,nodeId2] : #Ajouter nodeId3 en 3D
-		if boundaryNodes.count(j)==0 : #seuls les noeuds intérieurs contribuent au système linéaire (matrice de rigidité et second membre)
-			j_int=interiorNodes.index(j)#indice du noeud j en tant que noeud intérieur
-			#Ajout de la contribution de la cellule triangulaire i au second membre du noeud j
-			RHS[j_int]=Ci.getMeasure()/3*my_RHSfield[j]+RHS[j_int] # intégrale dans le triangle du produit f x fonction de base
-			#Contribution de la cellule triangulaire i à la ligne j_int du système linéaire
- 			for k in [nodeId0,nodeId1,nodeId2] : #Ajouter nodeId3 en 3D
-				if boundaryNodes.count(k)==0 : #seuls les noeuds intérieurs contribuent à la matrice du système linéaire
-					k_int=interiorNodes.index(k)#indice du noeud k en tant que noeud intérieur
-					coeff = Rigidite(j_int,k_int)+GradShapeFuncs[j]*GradShapeFuncs[k]/Ci.getMeasure()
-					Rigidite.setValue(j_int,k_int,coeff)
-				#else: si condition limite non nulle au bord, ajouter la contribution du bord au second membre de la cellule j
+    # Remplissage de  la matrice de rigidité et du second membre
+    for j in [nodeId0,nodeId1,nodeId2] : #Ajouter nodeId3 en 3D
+        if boundaryNodes.count(j) == 0 : #seuls les noeuds intérieurs contribuent au système linéaire (matrice de rigidité et second membre)
+            j_int = interiorNodes.index(j)#indice du noeud j en tant que noeud intérieur
+            #Ajout de la contribution de la cellule triangulaire i au second membre du noeud j
+            RHS[j_int] = Ci.getMeasure()/3*my_RHS_field[j]+RHS[j_int] # intégrale dans le triangle du produit f x fonction de base
+            #Contribution de la cellule triangulaire i à la ligne j_int du système linéaire
+            for k in [nodeId0,nodeId1,nodeId2] : #Ajouter nodeId3 en 3D
+                if boundaryNodes.count(k) == 0 : #seuls les noeuds intérieurs contribuent à la matrice du système linéaire
+                    k_int = interiorNodes.index(k)#indice du noeud k en tant que noeud intérieur
+                    coeff = rigidite(j_int,k_int)+GradShapeFuncs[j]*GradShapeFuncs[k]/Ci.getMeasure()
+                    rigidite.setValue(j_int,k_int,coeff)
+                #else: si condition limite non nulle au bord, ajouter la contribution du bord au second membre de la cellule j
 
 print("Fin remplissage matrice de rigidité")
 
 # Résolution du système linéaire
-#=================================
-LS=cdmath.LinearSolver(Rigidite,RHS,100,1.E-6,"CG","ILU")#Remplacer CG par CHOLESKY pour solveur direct
-SolSyst=LS.solve()
+#===============================
+LS = cdmath.LinearSolver(rigidite,RHS,100,1.E-6,"CG","ILU") #Remplacer CG par CHOLESKY pour solveur direct
+SolSyst = LS.solve()
 
 print("Fin de la résolution du système linéaire")
 
 # Création du champ résultat
 #===========================
-my_ResultField = cdmath.Field("Result field", cdmath.NODES, my_mesh, 1)
+my_result_field = cdmath.Field("Result field", cdmath.NODES, my_mesh, 1)
 for j in range(nbInteriorNodes):
-    my_ResultField[interiorNodes[j]]=SolSyst[j];#remplissage des valeurs pour les noeuds intérieurs
+    my_result_field[interiorNodes[j]] = SolSyst[j]; #remplissage des valeurs pour les noeuds intérieurs
 for j in range(nbBoundaryNodes):
-    my_ResultField[boundaryNodes[j]]=0;#remplissage des valeurs pour les noeuds frontière (condition limite)
-#sauvegarde sur le disque dur du résultat dans un fichier paraview
-my_ResultField.writeVTK("FiniteElementsResultField")
+    my_result_field[boundaryNodes[j]] = 0; #remplissage des valeurs pour les noeuds frontière (condition limite)
+# Sauvegarde sur le disque dur du résultat dans un fichier Paraview
+my_result_field.writeVTK("FiniteElementsResultField")
 
 print("Fin de la résolution numérique EF du problème de poisson 2D")
 
-#Calcul de l'erreur commise par rapport à la solution exacte
-#===========================================================
-max_sol_exacte=(my_RHSfield.getNormEuclidean()).max()/(2*pi*pi)
-erreur_max=(my_RHSfield/(2*pi*pi) - my_ResultField).getNormEuclidean().max()
+# Calcul de l'erreur commise par rapport à la solution exacte
+#============================================================
+max_sol_exacte = (my_RHS_field.getNormEuclidean()).max()/(2*pi*pi)
+erreur_max = (my_RHS_field/(2*pi*pi) - my_result_field).getNormEuclidean().max()
 print("max(| solution numerique - solution exacte |)/max(| solution exacte |) = ",erreur_max/max_sol_exacte)
 
-#Postprocessing optionnel: ouverture du fichier FiniteElementsResultField.pvd contenant le résultat numérique à partir de commandes python (import paraview)
+# Postprocessing optionnel: ouverture du fichier FiniteElementsResultField.pvd
+# contenant le résultat numérique à partir de commandes python (import paraview)
--- examples/poisson/finite_volumes.py
+++ examples/poisson/finite_volumes.py
@@ -1,28 +1,36 @@
+#!/usr/bin/env python
 # -*-coding:utf-8 -*
-#===============================================================================================================================
-# Name        : Résolution VF de l'équation de Poisson -\triangle u = f avec conditions aux limites de Dirichlet u=0
-# Author      : Michaël Ndjinga
-# Copyright   : CEA Saclay 2016
-# Description : Utilisation de la méthode des volumes finis avec champs u et f discrétisés aux cellules d'un maillage quelconque
-#				Création et sauvegarde du champ résultant ainsi que du champ second membre en utilisant CDMATH
-#================================================================================================================================
+
+#===============================================================================
+# Name       : Résolution VF de l'équation de Poisson -Δu = f
+#                  avec conditions aux limites de Dirichlet u=0.
+# Author     : Michaël Ndjinga
+# Copyright  : CEA Saclay 2016
+# Description: Utilisation de la méthode des volumes finis
+#                  avec champs u et f discrétisés aux cellules
+#                  d'un maillage quelconque.
+#                  Création et sauvegarde du champ résultant ainsi que
+#                  du champ second membre en utilisant CDMATH.
+#===============================================================================
+
+from math import pi
+from math import sin
 
 import cdmath
-from math import sin, pi
 
 # Création d'un maillage cartésien du domaine carré [0,1]x[0,1], définition des bords
 #====================================================================================
-xmin=0
-xmax=1
-ymin=0
-ymax=1
-#zmin=0 si calcul 3D
-#zmax=1 si calcul 3D
-nx=51
-ny=51
-#nz=50 si calcul 3D
+xmin = 0
+xmax = 1
+ymin = 0
+ymax = 1
+#zmin = 0 si calcul 3D
+#zmax = 1 si calcul 3D
+nx = 51
+ny = 51
+#nz = 50 si calcul 3D
 my_mesh = cdmath.Mesh(xmin,xmax,nx,ymin,ymax,ny)#Ajouter ,zmin,zmax,nz si calcul 3D
-eps=1e-6
+eps = 1e-6
 my_mesh.setGroupAtPlan(0,0,eps,"DirichletBorder")#Bord GAUCHE
 my_mesh.setGroupAtPlan(1,0,eps,"DirichletBorder")#Bord DROIT
 my_mesh.setGroupAtPlan(0,1,eps,"DirichletBorder")#Bord BAS
@@ -35,70 +43,70 @@ nbCells = my_mesh.getNumberOfCells()
 print("Fin construction du maillage")
 print("nb of cells ",nx,"x",ny, " = ", nbCells)
 
-#Discrétisation du second membre et détermination des noeuds intérieurs
-#======================================================================
-my_RHSfield = cdmath.Field("RHS field", cdmath.CELLS, my_mesh, 1)
-maxNbNeighbours=0#This is to determine the number of non zero coefficients in the sparse finite element rigidity matrix
+# Discrétisation du second membre et détermination des noeuds intérieurs
+#=======================================================================
+my_RHS_field = cdmath.Field("RHS field", cdmath.CELLS, my_mesh, 1)
+maxNbNeighbours = 0#This is to determine the number of non zero coefficients in the sparse finite element rigidity matrix
 #parcours des cellules pour discrétisation du second membre et extraction du nb max de voisins d'une cellule
 for i in range(nbCells):
-	Ci = my_mesh.getCell(i)
-	x = Ci.x()
-	y = Ci.y()
-	# z=Ci.z() si calcul 3D
-	my_RHSfield[i]=2*pi*pi*sin(pi*x)*sin(pi*y)#mettre la fonction definie au second membre de l edp
-	# compute maximum number of neighbours
-	maxNbNeighbours= max(1+Ci.getNumberOfFaces(),maxNbNeighbours)
+    Ci = my_mesh.getCell(i)
+    x = Ci.x()
+    y = Ci.y()
+    # z = Ci.z() si calcul 3D
+    my_RHS_field[i] = 2*pi*pi*sin(pi*x)*sin(pi*y)#mettre la fonction definie au second membre de l edp
+    # compute maximum number of neighbours
+    maxNbNeighbours = max(1+Ci.getNumberOfFaces(),maxNbNeighbours)
 
 # sauvegarde sur le disque dur du second membre discrétisé dans un fichier paraview
-my_RHSfield.writeVTK("FiniteVolumesRHSField")
+my_RHS_field.writeVTK("FiniteVolumesRHSField")
 
 print("Fin de la discrétisation du second membdre de l'edp")
-print("Max nb of neighbours=", maxNbNeighbours)
+print("Max nb of neighbours = ", maxNbNeighbours)
 
 # Construction de la matrice de rigidité et du vecteur second membre du système linéaire
 #=======================================================================================
-Rigidite=cdmath.SparseMatrix(nbCells,nbCells,nbCells*maxNbNeighbours)
-RHS=cdmath.Vector(nbCells)
+rigidite = cdmath.SparseMatrix(nbCells,nbCells,nbCells*maxNbNeighbours)
+RHS = cdmath.Vector(nbCells)
 #Parcours des cellules du domaine
 for i in range(nbCells):
-	RHS[i]=my_RHSfield[i] #la valeur moyenne du second membre f dans la cellule i
-	Ci=my_mesh.getCell(i)
-	for j in range(Ci.getNumberOfFaces()):# parcours des faces voisinnes
-		Fj=my_mesh.getFace(Ci.getFaceId(j))
-		coeff=Fj.getMeasure()/Ci.getMeasure()
-		if not Fj.isBorder():
-			k=Fj.getCellId(0)
-			if k==i :
-				k=Fj.getCellId(1)
-			Ck=my_mesh.getCell(k)
-			distance=Ci.getBarryCenter().distance(Ck.getBarryCenter())
-			Rigidite.setValue(i,k,-coeff/distance) # terme extradiagonal
-		coeff=coeff/2/Ci.getBarryCenter().distance(Fj.getBarryCenter()) + Rigidite(i,i) #on ajoute la contribution sur la diagonale
-		Rigidite.setValue(i,i,coeff) # terme diagonal
+    RHS[i] = my_RHS_field[i] #la valeur moyenne du second membre f dans la cellule i
+    Ci = my_mesh.getCell(i)
+    for j in range(Ci.getNumberOfFaces()):# parcours des faces voisinnes
+        Fj = my_mesh.getFace(Ci.getFaceId(j))
+        coeff = Fj.getMeasure()/Ci.getMeasure()
+        if not Fj.isBorder():
+            k = Fj.getCellId(0)
+            if k == i :
+                k = Fj.getCellId(1)
+            Ck = my_mesh.getCell(k)
+            distance = Ci.getBarryCenter().distance(Ck.getBarryCenter())
+            rigidite.setValue(i,k,-coeff/distance) # terme extradiagonal
+        coeff = coeff/2/Ci.getBarryCenter().distance(Fj.getBarryCenter()) + rigidite(i,i) #on ajoute la contribution sur la diagonale
+        rigidite.setValue(i,i,coeff) # terme diagonal
 
 print("Fin du remplissage matrice de rigidité")
 
 # Résolution du système linéaire
 #=================================
-LS=cdmath.LinearSolver(Rigidite,RHS,500,1.E-6,"CG","ILU")
-SolSyst=LS.solve()
+LS = cdmath.LinearSolver(rigidite,RHS,500,1.E-6,"CG","ILU")
+SolSyst = LS.solve()
 
 print("Fin de la résolution du système linéaire")
 
 # Création du champ résultat
 #===========================
-my_ResultField = cdmath.Field("Result field", cdmath.CELLS, my_mesh, 1)
+my_result_field = cdmath.Field("Result field", cdmath.CELLS, my_mesh, 1)
 for i in range(nbCells):
-    my_ResultField[i]=SolSyst[i];
+    my_result_field[i] = SolSyst[i];
 #sauvegarde sur le disque dur du résultat dans un fichier paraview
-my_ResultField.writeVTK("FiniteVolumesResultField")
+my_result_field.writeVTK("FiniteVolumesResultField")
 
 print("Fin de la résolution numérique VF du problème de poisson 2D")
 
 #Calcul de l'erreur commise par rapport à la solution exacte
 #===========================================================
-max_sol_exacte=(my_RHSfield.getNormEuclidean()).max()/(2*pi*pi)
-erreur_max=(my_RHSfield/(2*pi*pi) - my_ResultField).getNormEuclidean().max()
+max_sol_exacte = (my_RHS_field.getNormEuclidean()).max()/(2*pi*pi)
+erreur_max = (my_RHS_field/(2*pi*pi) - my_result_field).getNormEuclidean().max()
 print("max(| solution numerique - solution exacte |)/max(| solution exacte |) = ",erreur_max/max_sol_exacte)
 
 #Postprocessing optionnel: ouverture du fichier FiniteElementsResultField.pvd contenant le résultat numérique à partir de commandes python (import paraview)
